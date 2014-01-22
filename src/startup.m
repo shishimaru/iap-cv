6caf5ee5852fa506770c40f0573d888ec3838536
chikao <tsuchiya@csail.mit.edu>
2014-01-22 14:07:06 -0500

    fixed a bug in startup.m

--- src/startup.m
+++ src/startup.m
@@ -1,5 +1,5 @@
 % Start up script that is automatically executed when MATLAB is started.
 
-addpath('devkit');
-addpath('feature-extraction');
-addpath('liblinear-1.94');
+addpath('../devkit');
+addpath('../feature-extraction');
+addpath('../liblinear-1.94');
