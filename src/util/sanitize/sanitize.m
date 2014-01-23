b12c895d0dbfcb6b501fb25e51cc53dc26ed6c30
chikao <tsuchiya@csail.mit.edu>
2014-01-23 00:25:06 -0500

    Fixed some bugs

--- src/util/sanitize/sanitize.m
+++ src/util/sanitize/sanitize.m
@@ -1,4 +1,5 @@
-function sanitize(cls)
+function [model] = sanitize(cls)
+
 dataset_folder = '../viscomp/';
 devkit_folder  = '../devkit/';
 cache_folder   = '../cache/';
@@ -14,25 +15,25 @@ load(fullfile(devkit_folder, 'classes.mat'));
 fprintf('loading annotations...');
 filename = fullfile(cache_folder, 'annotations_train.mat');
 if exist(filename, 'file')
-    load(filename);
+   load(filename);
 else
-    annotations = cellfun(@(x) VOCreadxml([dataset_folder '/' x]), train_data.annotations', 'UniformOutput', false);
-    save(filename, 'annotations');
+   annotations = cellfun(@(x) VOCreadxml([dataset_folder '/' x]), train_data.annotations', 'UniformOutput', false);
+   save(filename, 'annotations');
 end
 fprintf('done\n');
 
-%% Extract features
-num_train = length(annotations);
-num_train = 10;
+%% Extract features for dictionary
+num_imgs = length(annotations);
+num_imgs = 10;
 c = conf();
 
-fprintf('extracting features...');
-filename = fullfile(cache_folder, 'descriptors_train.mat');
+fprintf('extracting features for dictionary...');
+filename = fullfile(cache_folder, 'descriptors.mat');
 if exist(filename, 'file')
     load(filename);
 else
-    descriptors = cell(num_train, 1);
-    for i = 1:num_train
+    descriptors = zeros(0, 0);
+    for i = 1:num_imgs
         % Read an image
         img = imread(fullfile(dataset_folder, dataset, 'images', [annotations{i}.annotation.filename '.jpg']));
 
@@ -41,35 +42,25 @@ else
             img = repmat(img, [1 1 3]);
         end
 
-        % Crop images
-        for j = 1:3
-            try
+        for j = 1:length(annotations{i}.annotation.object)
+            % Crop images
             xmin = round(str2double(annotations{i}.annotation.object(j).bndbox.xmin)+1);
             xmax = round(str2double(annotations{i}.annotation.object(j).bndbox.xmax)+1);
             ymin = round(str2double(annotations{i}.annotation.object(j).bndbox.ymin)+1);
             ymax = round(str2double(annotations{i}.annotation.object(j).bndbox.ymax)+1);
-            img_crop{j} = img(ymin:ymax,xmin:xmax,:);
-            catch
-                keyboard;
-            end
+            img_crop = img(ymin:ymax,xmin:xmax,:);
+
+            % Extract descriptors
+            feat = extract_feature(feature, img_crop, c);
+            r = randperm(size(feat, 1));
+            feat_shrinked = feat(r(1:min(length(r), desc_per_img)), :);
+            descriptors(end+1:end+size(feat_shrinked,1),:) = feat_shrinked;
         end
 
-        % Extract features
-        feat = zeros(0,0);
-        for j = 1:3
-            tmp = extract_feature(feature, img_crop{j}, c);
-            feat = [feat; tmp];
-        end
-
-        % Shrink features
-        r = randperm(size(feat, 1));
-        descriptors{i} = feat(r(1:min(length(r), desc_per_img)), :);
         fprintf('.');
     end
     save(filename, 'descriptors');
 end
-all_descriptors = cell2mat(descriptors);
-num_data = size(all_descriptors, 1);
 fprintf('done\n');
 
 %% Whiten data
@@ -77,7 +68,7 @@ fprintf('done\n');
 
 %% Build a dictionary
 fprintf('building a dictionary...');
-filename = fullfile(cache_folder, 'dictionary_train.mat');
+filename = fullfile(cache_folder, 'dictionary.mat');
 if exist(filename, 'file')
     load(filename);
 else
@@ -86,42 +77,66 @@ else
 end
 fprintf('done\n');
 
-% Calculate features for SVM training
-Xall = zeros(0,0);
-Yall = zeros(0,0);
-for i = 1:num_train
-    Yall(end+1) = str2double(annotations{i}.annotation.classes.(cls));
-    
-    desc = descriptors{i};
-    num_desc = size(desc, 1);
-    x = zeros(1, dic_size);
-    for j = 1:num_desc
-        % Find the closest centroid
-        closest_k = 0;
-        min_dist = inf;
-        for k = 1:dic_size
-            dist = dictionary(k,:) - desc(j,:);
-            dist = sum(dist .^ 2);
-            if dist < min_dist
-                min_dist = dist;
-                closest_k = k;
-            end
+%% Extract features for SVM training
+fprintf('extracting features for SVM training...');
+filename = fullfile(cache_folder, 'training_data.mat');
+if exist(filename, 'file')
+    load(filename);
+else
+    Xall = zeros(0,0);
+    Yall = zeros(0,0);
+    for i = 1:num_imgs
+        % Read an image
+        img = imread(fullfile(dataset_folder, dataset, 'images', [annotations{i}.annotation.filename '.jpg']));
+
+        % Make sure the image is color
+        if size(img, 3) == 1
+            img = repmat(img, [1 1 3]);
         end
-        x(closest_k) = x(closest_k) + 1;
+
+        for j = 1:length(annotations{i}.annotation.object)
+            % Crop images
+            xmin = round(str2double(annotations{i}.annotation.object(j).bndbox.xmin)+1);
+            xmax = round(str2double(annotations{i}.annotation.object(j).bndbox.xmax)+1);
+            ymin = round(str2double(annotations{i}.annotation.object(j).bndbox.ymin)+1);
+            ymax = round(str2double(annotations{i}.annotation.object(j).bndbox.ymax)+1);
+            img_crop = img(ymin:ymax,xmin:xmax,:);
+
+            % Extract descriptors
+            feat = extract_feature(feature, img_crop, c);
+            x = zeros(1, dic_size);
+            for k = 1:size(feat, 1)
+                % Find the closest centroid
+                closest_l = 0;
+                min_dist = inf;
+                for l = 1:dic_size
+                    dist = dictionary(l,:) - feat(k,:);
+                    dist = sum(dist .^ 2);
+                    if dist < min_dist
+                        min_dist = dist;
+                        closest_l = l;
+                    end
+                end
+                x(closest_l) = x(closest_l) + 1;
+            end
+            x = x ./ (sum(x) + eps); % normalize
+            Xall(end+1,:) = x;
+
+            % Get a label based on the annotation
+            Yall(end+1,1) = str2double(annotations{i}.annotation.classes.(cls));
+        end
     end
-    x = x ./ (sum(x) + eps); % normalize
-    Xall(end+1,:) = x;
+    save(filename, 'Xall', 'Yall');
 end
+fprintf('done\n');
 
-% Train a model
+%% Train a model
 svm_options = '-s 2 -B 1 -c 1 -q';
-model = train(Yall', sparse(Xall), svm_options);
+model = train(Yall, sparse(Xall), svm_options);
 
-% Calculate confidences
-for i = 1:size(Xall,1);
-    confidence = [Xall(i,:), 1] * model.w';
-    fprintf('conf = %f, label = %d\n', confidence, Yall(i));
-end
+% Calculate confidences for the training data
+svm_options = '-b 1';
+[predicted_label, accuracy, prob] = predict(Yall, sparse(Xall), model, svm_options);
 
 
 end
