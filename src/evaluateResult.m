37b1fdaaf18a1a3455c47c6f9f92903c6ac6a1a9
Hitoshi Uchida <uchida@w3.org>
2014-01-23 12:08:47 -0800

    Added evaluation function.

---
+++ src/evaluateResult.m
@@ -0,0 +1,33 @@
+function evaluateResult(predicted_labels)
+  dataset_folder = '../viscomp/';
+  devkit_folder  = '../devkit/';
+  cache_folder   = '../cache/';
+
+  load(fullfile(devkit_folder, 'classes.mat'));
+  load(fullfile(devkit_folder, 'filelists.mat'));
+  
+  fprintf('loading annotations...');
+  filename = '../cache/annotations_val.mat';
+  if exist(filename, 'file')
+    load(filename);
+  else
+    annotations_val = cellfun(@(x) VOCreadxml([dataset_folder x]), val_data.annotations', 'UniformOutput', false);
+    save(filename, 'annotations_val');
+  end
+  fprintf('done\n');
+
+  average_precision = zeros(length(classes), 1);
+
+  %% DEBUG
+  annotations_val = annotations_val(1:300,:);
+  
+  % compute average precision, using rand to predict the confidence for each image
+  for i=1:length(classes)
+    ground_truth_labels = cellfun(@(x) str2double(x.annotation.classes.(classes{i})),annotations_val);
+    
+    % predicted_labels = rand(size(annotations_val));
+    average_precision(i) = computeAP(predicted_labels(i,:)', ground_truth_labels, 1)*100;
+    fprintf('class: %s, average precision: %.02f%%\n', classes{i}, average_precision(i));
+  end
+  fprintf('mean average precision: %.02f%%\n', mean(average_precision));
+end
