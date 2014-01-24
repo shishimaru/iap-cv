2f2854c13021617b2c05c09903eaf20283c53374
Hitoshi Uchida <uchida@w3.org>
2014-01-23 21:54:59 -0800

    Added serializer to create upload file.

---
+++ src/upload/serialize.m
@@ -0,0 +1,47 @@
+function [] = serialize(confidence, dataName)
+% confidence : [(# of image), (# of classes)]
+% dataName   : 'test'(default) or 'val'
+
+if(nargin == 1), dataName = 'test'; end
+
+% IMPORTANT: UPDATE DATASET FOLDER
+dataset_folder = '../viscomp/';
+devkit_folder = '../devkit/';
+cache_folder = '../cache/';
+
+% load information about test data
+fprintf('loading classes...');
+load('classes.mat');
+
+% check error of class size
+if(size(confidence, 2) ~= length(classes)), 
+    fprintf('Dimention of confidence must be same to classes %d\n', length(classes));
+    pause;
+end
+
+% load annotations
+fprintf('loading annotations_%s.mat...', dataName);
+load(fullfile(cache_folder, sprintf('annotations_%s.mat', dataName)));
+if(strcmp(dataName, 'val') == 1)
+    annotations = annotations_val;
+elseif(strcmp(dataName, 'test') == 1)
+    annotations = annotations_test;
+end
+fprintf('done\n');
+
+% check error of vector size
+if(size(confidence, 1) > length(annotations)),
+    fprintf('Vector size of confidence is larger than one of annotations\n');
+    pause;
+end
+
+% get the list of image names for test data as shown below
+annotations = annotations(1:size(confidence, 1), :);
+image_names = cellfun(@(x) x.annotation.filename, annotations, 'UniformOutput', false);
+
+% start to serialize the confidence
+upload_file_name = sprintf('util/upload/upload_%s_%d.txt', dataName, size(confidence,1));
+fprintf('serializing %s\n', upload_file_name);
+createUploadFile(image_names, classes, confidence, upload_file_name);
+fprintf('done\n');
+end
