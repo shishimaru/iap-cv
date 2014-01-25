7ff5969100a889442a6f3d40dc7568f6b772506a
chikao <tsuchiya@csail.mit.edu>
2014-01-25 18:58:55 -0500

    Added featureNormalize.m for feature nomalization.

---
+++ src/featureNormalize.m
@@ -0,0 +1,21 @@
+function [X_norm, mu, sigma] = featureNormalize(X, mu, sigma)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+
+if ~exist('mu', 'var')
+    mu = mean(X);
+    X_norm = bsxfun(@minus, X, mu);
+
+    sigma = std(X_norm);
+    X_norm = bsxfun(@rdivide, X_norm, sigma);
+else
+    X_norm = bsxfun(@minus, X, mu);
+    X_norm = bsxfun(@rdivide, X_norm, sigma);
+end
+
+% ============================================================
+
+end
